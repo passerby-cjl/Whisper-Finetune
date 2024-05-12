3f534af5aeee0c97faf370eca8715872d9003aba
passerby-cjl <87693336+passerby-cjl@users.noreply.github.com>
2024-05-12 14:48:22 +0800

    reduce log info

--- aishell.py
+++ aishell.py
@@ -49,7 +49,6 @@ def create_annotation_text(data_dir, annotation_path):
         text = ''.join(text.split())
         if args.add_pun:
             text = inference_pipeline(input=text)
-            print("result len", len(text))
             text = text[0]
         transcript_dict[audio_id] = text
     # 训练集
